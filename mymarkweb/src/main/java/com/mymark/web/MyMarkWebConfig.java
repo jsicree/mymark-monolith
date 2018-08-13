f1938053928ee1bba79c835820e3d157e3f4d5fe
Joseph Sicree <joseph.sicree@cgi.com>
2018-08-13 17:08:39 -0400

    Adding Bootstrap and Thymeleaf

--- mymarkweb/src/main/java/com/mymark/web/MyMarkWebConfig.java
+++ mymarkweb/src/main/java/com/mymark/web/MyMarkWebConfig.java
@@ -5,84 +5,104 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafView;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = { "com.mymark.web.controllers" })
 public class MyMarkWebConfig implements WebMvcConfigurer {
  
-//   @Override
-//   public void addViewControllers(ViewControllerRegistry registry) {
-//      registry.addViewController("/sample.html");
-//   }
-// 
-//   @Bean
-//   public ViewResolver viewResolver() {
-//      InternalResourceViewResolver bean = new InternalResourceViewResolver();
-// 
-//      bean.setViewClass(JstlView.class);
-//      bean.setPrefix("/WEB-INF/view/");
-//      bean.setSuffix(".jsp");
-// 
-//      return bean;
-//   }
-
-   /*
-    * STEP 1 - Create SpringResourceTemplateResolver
-    * */
-
 	@Autowired
 	   private ApplicationContext applicationContext;
 	
-   @Bean
-   public SpringResourceTemplateResolver templateResolver() {
-      SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-      templateResolver.setApplicationContext(applicationContext);
-      templateResolver.setPrefix("/WEB-INF/view/");
-      templateResolver.setSuffix(".html");
-      return templateResolver;
-   }
+    /*
+     *  Dispatcher configuration for serving static resources
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations(
+                        "classpath:/static/css/");
+        registry.addResourceHandler("/images/**")
+        		.addResourceLocations("classpath:/static/images/");
+        registry.addResourceHandler("/js/**")
+        		.addResourceLocations("classpath:/static/js/");
+        
+    }
+    
+    /*
+     *  Message externalization/internationalization
+     */
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("Messages");
+        return messageSource;
+    }
 
-   /*
-    * STEP 2 - Create SpringTemplateEngine
-    * */
-   @Bean
-   public SpringTemplateEngine templateEngine() {
-      SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-      templateEngine.setTemplateResolver(templateResolver());
-      templateEngine.setEnableSpringELCompiler(true);
-      return templateEngine;
-   }
 
-   /*
-    * STEP 3 - Register ThymeleafViewResolver
-    * */
-   @Override
-   public void configureViewResolvers(ViewResolverRegistry registry) {
-      ThymeleafViewResolver resolver = new ThymeleafViewResolver();
-      resolver.setTemplateEngine(templateEngine());
-      registry.viewResolver(resolver);
-   }   
-   
-   @Override
-   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-       registry.addResourceHandler(
-               "/css/**")
-               .addResourceLocations(
-                       "classpath:/static/css/");
-   }
 
-//	@Override
-//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-//	}
-   
+    /* **************************************************************** */
+    /*  THYMELEAF-SPECIFIC ARTIFACTS                                    */
+    /*  TemplateResolver <- TemplateEngine <- ViewResolver              */
+    /* **************************************************************** */
+
+    // SpringResourceTemplateResolver
+	@Bean
+    public SpringResourceTemplateResolver templateResolver(){
+        // SpringResourceTemplateResolver automatically integrates with Spring's own
+        // resource resolution infrastructure, which is highly recommended.
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(this.applicationContext);
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        // HTML is the default value, added here for the sake of clarity.
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        // Template cache is true by default. Set to false if you want
+        // templates to be automatically updated when modified.
+        templateResolver.setCacheable(true);
+        return templateResolver;
+    }
+	
+	// TemplateEngine
+	@Bean
+    public SpringTemplateEngine templateEngine(){
+        // SpringTemplateEngine automatically applies SpringStandardDialect and
+        // enables Spring's own MessageSource message resolution mechanisms.
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        // Enabling the SpringEL compiler with Spring 4.2.4 or newer can
+        // speed up execution in most scenarios, but might be incompatible
+        // with specific cases when expressions in one template are reused
+        // across different data types, so this flag is "false" by default
+        // for safer backwards compatibility.
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+	
+	// Thymeleaf View Resolver
+    @Bean
+    public ThymeleafViewResolver viewResolver(){
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        return viewResolver;
+    }	
+
+	// ThymeleafView
+	@Bean
+	public ThymeleafView thymeleafView() {
+		ThymeleafView bean = new ThymeleafView();
+		bean.addStaticVariable("footer", "&lt;b&gt;MyMark Mono 2018&lt;/b&gt;");
+		return bean;
+	}
+
    
 }
--- mymarkweb/src/main/java/com/mymark/web/controllers/HomeController.java
+++ mymarkweb/src/main/java/com/mymark/web/controllers/HomeController.java
@@ -2,10 +2,10 @@ package com.mymark.web.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
@@ -14,7 +14,7 @@ public class HomeController {
 			.getLogger(HomeController.class);
 
 		@GetMapping("/home")
-		public String welcome(Model model) {
+		public String home(Model model) {
 			log.info("Displaying the home page.");
 		    model.addAttribute("message", "Welcome to Fantasy Island!");
 			return "home";
